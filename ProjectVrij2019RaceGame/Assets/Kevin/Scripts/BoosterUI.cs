951fc2433bed8bca48cc82c4a1631dfccfab8132
Unknown <kevin.absen@hotmail.com>
2019-05-26 21:01:27 +0200

    Booster UI, defend and attack UI (first version) and timer added

---
+++ ProjectVrij2019RaceGame/Assets/Kevin/Scripts/BoosterUI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class BoosterUI : MonoBehaviour {
+
+    public Image boosterBarLeft;
+    public Image boosterBarRight;
+    public TextMeshProUGUI boosterText;
+    public Boost boost;
+
+    void Update() {
+        BoostBar();
+    }
+
+    private void BoostBar() {
+        boosterBarLeft.fillAmount = boost.currentBoostLevel / boost.maxBoostLevel;
+        boosterBarRight.fillAmount = boost.currentBoostLevel / boost.maxBoostLevel;
+        boosterText.text = Mathf.RoundToInt(Mathf.Clamp(boost.currentBoostLevel, 0, boost.maxBoostLevel)).ToString() + "%";
+    }
+}
