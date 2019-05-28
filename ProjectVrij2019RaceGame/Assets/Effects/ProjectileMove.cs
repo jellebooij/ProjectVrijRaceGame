8a37a09cd11819ccddfe51ceed38e33ab7f01f9f
Unknown <mickteun@gmail.com>
2019-05-28 20:48:57 +0200

    Effects er in, render pipeline, audio
    schrik niet als de materials paars zijn ik heb de render pipeline er in gezet

---
+++ ProjectVrij2019RaceGame/Assets/Effects/ProjectileMove.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileMove : MonoBehaviour
+{
+
+    public float speed;
+    public float fireRate;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (speed != 0) {
+            transform.position += transform.forward * (speed * Time.deltaTime);
+            StartCoroutine("DeleteProjectile");
+
+        } else {
+            Debug.Log("No Speed");
+        }
+        
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        speed = 0;
+
+        Destroy(gameObject);
+         
+    }
+
+    IEnumerator DeleteProjectile() {
+
+        yield return new WaitForSeconds(3f);
+
+        Object.Destroy(gameObject);
+
+    }
+}
---
+++ ProjectVrij2019RaceGame/Assets/Effects/SpawnProjectiles.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnProjectiles : MonoBehaviour {
+
+    public GameObject firePoint;
+    public List<GameObject> vfx = new List<GameObject>();
+    public RotateToMouse rotateToMouse;
+
+    private GameObject effectSpawn;
+    private float timeToFire = 0;
+
+    void Start() {
+        effectSpawn = vfx[0];
+        
+    }
+
+    void Update() {
+        if(Input.GetMouseButton(0) && Time.time >= timeToFire) {
+            timeToFire = Time.time + 1 / effectSpawn.GetComponent<ProjectileMove>().fireRate;
+            SpawnVFX();
+            
+        }
+
+        
+    }
+
+    void SpawnVFX(){
+
+        GameObject vfx;
+
+        if (firePoint !=null) {
+            vfx = Instantiate(effectSpawn, firePoint.transform.position, Quaternion.identity);
+
+            if (rotateToMouse != null) {
+                vfx.transform.localRotation = rotateToMouse.GetRotation();
+
+            }
+
+        } else {
+            Debug.Log("No Fire Point");
+        }
+    }
+
+
+
+}
