d6a7be49735152f4038286b72b748926aa880bd0
jellebooij <jellebooij@outlook.com>
2019-06-04 14:09:09 +0200

    zdb

--- ProjectVrij2019RaceGame/Assets/Scripts/Network/ClientBehaviour.cs
+++ ProjectVrij2019RaceGame/Assets/Scripts/Network/ClientBehaviour.cs
@@ -182,7 +182,7 @@ public class ClientBehaviour : MonoBehaviour
 
     void UpdateWorldState(){
 
-        float currentTime = time - 0.1f;
+        float currentTime = time - 0.5f;
 
         for(int i = 0; i < 10; i++){
             
