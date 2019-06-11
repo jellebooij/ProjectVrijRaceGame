d1678c93367635bebc527b10379378039186ba8e
Unknown <kevin.absen@hotmail.com>
2019-06-11 20:24:56 +0200

    Worldspace Player Health

---
+++ ProjectVrij2019RaceGame/Assets/Kevin/Scripts/WorldSpaceHealthUI.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class WorldSpaceHealthUI : MonoBehaviour {
+
+    public Image healthBarRight;
+    public Health health;
+
+    [SerializeField]
+    private float dangerLevel = 30f;
+
+    [SerializeField]
+    private Color fullColor;
+
+    [SerializeField]
+    private Color lowColor;
+
+    [SerializeField]
+    private Color whiteColor;
+
+    private void Update() {
+        HealthBar();
+    }
+
+    private void HealthBar() {
+        healthBarRight.fillAmount = health.health / health.maxHealth;
+
+        if (health.health < dangerLevel) {
+            healthBarRight.color = lowColor;
+        } else {
+            healthBarRight.color = fullColor;
+        }
+    }
+}
